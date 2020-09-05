2a4bd57fd52fa8fc86260705debacf58aecefb27
pntech20 <pntech20@gmail.com>
2020-09-05 19:45:35 +0700

    02. Type Basics

---
+++ 02. Type Basics/sandbox.js
@@ -0,0 +1,5 @@
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
+// console.log(circ('hello'))
---
+++ 02. Type Basics/sandbox.ts
@@ -0,0 +1,6 @@
+const circ = (diameter: number) => {
+  return diameter * Math.PI
+}
+
+console.log(circ(10))
+// console.log(circ('hello'))
