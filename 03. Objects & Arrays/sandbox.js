c73e3538ee8405ce18ea22720b0d48be2b6cd13a
pntech20 <pntech20@gmail.com>
2020-09-05 20:16:24 +0700

    03. Objects & Arrays

---
+++ 03. Objects & Arrays/sandbox.js
@@ -0,0 +1,36 @@
+var stringArr = ['one', 'two', 'three'];
+stringArr.push('four');
+// stringArr.push(4); // Error
+// stringArr = 'hola'; // Error
+var numArr = [1, 2, 3];
+numArr.push(4);
+// numArr.push('four'); // Error
+var mixArr = [1, 2, 'three'];
+mixArr.push(4);
+mixArr.push('four');
+mixArr[0] = 'zero';
+// mixArr.push(false) // Error
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja['name'] = 'new name';
+// ninja.age = '30' // Error
+// ninja.skills = ['fighting', 'sneaking'] // Error
+// ninja = [] // Error
+ninja = {
+    name: 'a',
+    belt: 'b',
+    age: 5
+};
+// ninja = {
+//   name: 'a',
+//   belt: 'b'
+// } // Error
+// ninja = {
+//   name: 'a',
+//   belt: 'b',
+//   newAttribute: 5
+// } // Error
